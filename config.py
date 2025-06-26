2fd07c0ff5821ed947c9e8ad05f1ec76f8d75231
Mariocuo <mario_1993@hotmail.it>
2025-06-27 01:10:39 +0200

    Re-enable Google Sheets integration

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbzUBbJVPRiVhtuY48aXoRZAgRoDKjncBfyrBJYLHOtRBQd7SE1UoS-nz0IhSQxfRp77Ww/exec"
 
 # Se vuoi disabilitare l'invio a Google Sheets, imposta a False
-ENABLE_GOOGLE_SHEETS = False 
+ENABLE_GOOGLE_SHEETS = True 
